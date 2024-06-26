5f68c46f4b35c7d9f35cfe8ddbc53bb2ff575a25
Luis Carretero Lopez <luiscarretero.tech@gmail.com>
2024-06-26 21:51:09 +0200

    Utils, thresholds and FSM added

---
+++ thresholds.py
@@ -0,0 +1,26 @@
+
+
+
+# Get thresholds for beginner mode
+def get_thresholds():
+
+    _ANGLE_HIP_KNEE_VERT = {
+                            'STAND' : (0,  10),
+                            'TRANS'  : (15, 80),
+                            'PASS'   : 90
+                        }    
+
+        
+    thresholds = {
+                    'HIP_KNEE_VERT': _ANGLE_HIP_KNEE_VERT,
+
+                    'KNEE_THRESH'  : [15, 80, 95],
+
+                    'OFFSET_THRESH'    : 35.0,
+                    # 'INACTIVE_THRESH'  : 15.0,
+
+                    'CNT_FRAME_THRESH' : 50
+                            
+                }
+                
+    return thresholds
