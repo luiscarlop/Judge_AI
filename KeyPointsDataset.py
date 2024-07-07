22acbb559865061aee160da48785e6cc3cb17f3c
Luis Carretero Lopez <luiscarretero.tech@gmail.com>
2024-07-07 23:13:42 +0200

    Modelo chungo

--- KeyPointsDataset.py
+++ KeyPointsDataset.py
@@ -15,7 +15,7 @@ import os
 
 BATCH_SIZE = 32
 IMG_SIZE = 224
-NUM_KEYPOINTS = 10
+NUM_KEYPOINTS = 10 * 2
 json_data = json.load(open("annotations.json"))
 
 def get_annotation(name: str):
@@ -30,7 +30,59 @@ def get_annotation(name: str):
 
     return data
 
-class KeyPointDataset(keras.utils.PyDataset):
+def visualize_keypoints(images, keypoints):
+    fig, axes = plt.subplots(nrows=len(images), ncols=2, figsize=(16, 12))
+    [ax.axis("off") for ax in np.ravel(axes)]
+
+    for (ax_orig, ax_all), image, current_keypoint in zip(axes, images, keypoints):
+        ax_orig.imshow(image)
+        ax_all.imshow(image)
+
+        # If the keypoints were formed by `imgaug` then the coordinates need
+        # to be iterated differently.
+        if isinstance(current_keypoint, KeypointsOnImage):
+            for idx, kp in enumerate(current_keypoint.keypoints):
+                ax_all.scatter(
+                    [kp.x],
+                    [kp.y],
+                    # c=colours[idx],
+                    marker="x",
+                    s=50,
+                    linewidths=5,
+                )
+        else:
+            current_keypoint = np.array(current_keypoint, dtype=np.float32).reshape(-1, 2)
+            # Since the last entry is the visibility flag, we discard it.
+            current_keypoint = current_keypoint[:, :2]
+            for idx, (x, y) in enumerate(current_keypoint):
+                ax_all.scatter([x], [y], marker="o", s=10, linewidths=1)
+
+    plt.tight_layout(pad=2.0)
+    plt.show()
+
+def get_model():
+    # Load the pre-trained weights of MobileNetV2 and freeze the weights
+    backbone = keras.applications.MobileNetV2(
+        weights="imagenet",
+        include_top=False,
+        input_shape=(IMG_SIZE, IMG_SIZE, 3),
+    )
+    backbone.trainable = False
+
+    inputs = layers.Input((IMG_SIZE, IMG_SIZE, 3))
+    x = keras.applications.mobilenet_v2.preprocess_input(inputs)
+    x = backbone(x)
+    x = layers.Dropout(0.3)(x)
+    x = layers.SeparableConv2D(
+        NUM_KEYPOINTS, kernel_size=5, strides=1, activation="relu"
+    )(x)
+    outputs = layers.SeparableConv2D(
+        NUM_KEYPOINTS, kernel_size=3, strides=1, activation="sigmoid"
+    )(x)
+
+    return keras.Model(inputs, outputs, name="keypoint_detector")
+
+class KeyPointsDataset(keras.utils.PyDataset):
     def __init__(
             self,
             image_keys,
@@ -93,7 +145,7 @@ class KeyPointDataset(keras.utils.PyDataset):
                 kp_temp.append(np.nan_to_num(keypoint.y))
 
             # More on why this reshaping later.
-            batch_keypoints[i,] = np.array(kp_temp).reshape(1, 1, 24 * 2)
+            batch_keypoints[i,] = np.array(kp_temp).reshape(1, 1, NUM_KEYPOINTS)
 
         # Scale the coordinates to [0, 1] range.
         batch_keypoints = batch_keypoints / IMG_SIZE
